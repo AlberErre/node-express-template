50baf9d587557872800f9db8ebe8985a039a1dd6
Alber Erre <arblugo@gmail.com>
2018-11-28 20:30:01 +0000

    add state interface

--- src/components/Hello.tsx
+++ src/components/Hello.tsx
@@ -1,12 +1,21 @@
-import * as React from 'react';
+import * as React from "react";
 
 export interface HelloProps {
   name: string;
 }
 
+export interface HelloState {
+  counter: number;
+  enabled: boolean;
+}
 
-export default class Hello extends React.Component<HelloProps, {}> {
+export default class Hello extends React.Component<HelloProps, HelloState> {
+
   render() {
-    return <h1>Hello {this.props.name}</h1>;
+    return (
+      <h1>
+        Hello {this.props.name}
+      </h1>
+    );
   }
 }
