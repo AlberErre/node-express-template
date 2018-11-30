f4d5234282e125e6d7014d084a0850c56a16769d
Alber Erre <arblugo@gmail.com>
2018-11-30 17:25:43 +0000

    add JS styling

--- src/components/NumberPicker.tsx
+++ src/components/NumberPicker.tsx
@@ -25,14 +25,57 @@ export default class NumberPicker extends React.Component<{}, NumberPickerState>
   }
 
   render() {
-    return <div className="number-picker">
-      <button className="number-picker-buttons" onClick={this.increaseNumber}>
-          +
+    return (
+      <div style={numberPickerStyle}>
+        <button style={numberPickerButton} onClick={this.increaseNumber}>
+            +
         </button>
-      <span className="number-picker-label">{this.state.counter}</span>
-      <button className="number-picker-buttons" onClick={this.decreaseNumber}>
-          -
+        <span style={numberPickerLabel}>{this.state.counter}</span>
+        <button style={numberPickerButton} onClick={this.decreaseNumber}>
+            -
         </button>
-      </div>;
+      </div>
+    );
   }
 }
+
+const numberPickerStyle: React.CSSProperties = {
+  width: '150px',
+  height: '32px',
+  display: 'flex',
+  alignItems: 'center',
+  position: 'relative',
+  border: '1px solid #EDEEF4',
+  boxShadow: '0 2px 1px 0 rgba(0, 0, 0, 0.03)',
+  color: '#385871',
+  borderRadius: '5px'
+};
+
+const numberPickerLabel: React.CSSProperties = {
+  width: '100px',
+  height: '32px',
+  textAlign: 'center',
+  padding: '0',
+  outline: 'none',
+  fontFamily: 'OpenSans-Bold, sans-serif',
+  color: '#385871',
+  borderRadius: '5px',
+  lineHeight: '30px'
+};
+
+const numberPickerButton: React.CSSProperties = {
+  backgroundColor: "#4CAF50",
+  border: "none",
+  color: "white",
+  width: "30px",
+  height: "30px",
+  textAlign: "center",
+  textDecoration: "none",
+  display: "inline-block",
+  borderRadius: "50%",
+  fontSize: "25px",
+  paddingBottom: "5px",
+  lineHeight: "25px",
+  cursor: "pointer",
+  outline: 'none'
+};
