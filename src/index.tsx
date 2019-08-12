fac69ecbc195cc0d7e2a6bba9a562e9e5020819c
Alber Erre <arblugo@gmail.com>
2019-08-12 17:25:09 +0100

    rm

--- src/index.tsx
+++
@@ -1,5 +0,0 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-import NumberPicker from './components/NumberPicker';
-
-ReactDOM.render(<NumberPicker />, document.getElementById('app'));
